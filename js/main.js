fb232e9f01d83c7b575e3156984f4910034f6d7d
Zoey <zcolglazier@wisc.edu>
2020-01-22 14:33:34 -0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Created by Zoey Colglazier, Spring 2020 //
+var firstDiv = document.getElementById('firstDiv');
+
+firstDiv.addEventListener('click', function (){
+  alert('Abracadabra!')
+});
+
+var secondDiv = document.getElementById('secondDiv');
+
+secondDiv.addEventListener('click', function(){
+   alert('Whizz!')
+});
+//
+// var thirdDiv = document.getElementById('thirdDiv');
+//
+// thirdDiv.addEventListener('click', function({
+//   alert('Bang!')
+// }));
+//
+// var fourthDiv = document.getElementById('fourthDiv');
+//
+// fourthDiv.addEventListener('click', function({
+//   alert('Shazam!')
+// }));
