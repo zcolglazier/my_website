63a5a143940e0098544f1ad638e8a594c98e29b2
Zoey <zcolglazier@wisc.edu>
2020-01-22 14:39:28 -0600

    Update main.js
    Fixed Alerts

--- js/main.js
+++ js/main.js
@@ -10,15 +10,15 @@ var secondDiv = document.getElementById('secondDiv');
 secondDiv.addEventListener('click', function(){
    alert('Whizz!')
 });
-//
-// var thirdDiv = document.getElementById('thirdDiv');
-//
-// thirdDiv.addEventListener('click', function({
-//   alert('Bang!')
-// }));
-//
-// var fourthDiv = document.getElementById('fourthDiv');
-//
-// fourthDiv.addEventListener('click', function({
-//   alert('Shazam!')
-// }));
+
+var thirdDiv = document.getElementById('thirdDiv');
+
+thirdDiv.addEventListener('click', function(){
+   alert('Bang!')
+});
+
+var fourthDiv = document.getElementById('fourthDiv');
+
+fourthDiv.addEventListener('click', function(){
+  alert('Shazam!')
+});
